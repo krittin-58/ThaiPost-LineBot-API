fae14ef237055bdf43c6cb0f026b88085d4e3035
krittin <krittin.j58@rsu.ac.th>
2019-09-29 18:38:35 +0700

    init project

---
+++ config.js
@@ -0,0 +1,9 @@
+{
+    "line": {
+      "channelAccessToken": "C+InCMmS3GgAAf/jDgNWM8+AGMjMtCdlBV13AKIr8kPItCX4MR/xMufZq4lZYM0niaKqBvrUYP/bBsUOgPCvY5AXCjokHWCNjAWGv8wXLrEvGUh12vfFwB3rVv/UFFTl9gKcrjSoToBBUJurzK05+AdB04t89/1O/w1cDnyilFU=",
+      "channelSecret": "66e9ed429392cee6e12326c7bf16621c"
+    },
+    "thaipost": {
+        "token": "GdY;AQVVCdS%K4RMZGM&P!UzJWT6ChVzBqP@MNBMP%CbA#YbMXLoWyK+Z1S^EMXLTFBnTRQgKCVZO?T#P=L*T6BuNnY2X0D*M3V7"
+    }
+}
